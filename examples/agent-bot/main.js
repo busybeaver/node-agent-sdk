b56d71060e9bacca6535eed6cda15bf780ff4d9b
Jan Mollner <jmollner@liveperson.com>
2017-07-18 14:20:05 -0400

    fixed typo in event name
    Signed-off-by: Jan Mollner <jmollner@liveperson.com>

--- examples/agent-bot/main.js
+++ examples/agent-bot/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /*
- * This demo extends MyCoolAgent with the specific reply logic: 
- * 
+ * This demo extends MyCoolAgent with the specific reply logic:
+ *
  * 1) Echo any new message from the consumer
- * 2) Close the conversation if the consumer message starts with '#close' 
- * 
+ * 2) Close the conversation if the consumer message starts with '#close'
+ *
  */
 
 const MyCoolAgent = require('./MyCoolAgent');
@@ -14,26 +14,26 @@ const echoAgent = new MyCoolAgent({
     accountId: process.env.LP_ACCOUNT,
     username: process.env.LP_USER,
     password: process.env.LP_PASS,
-    // For internal lp only use 
+    // For internal lp only use
     //  export LP_CSDS=hc1n.dev.lprnd.net
     csdsDomain: process.env.LP_CSDS
 });
 
-echoAgent.on('MyCoolAgent.ContentEvnet',(contentEvent)=>{
+echoAgent.on('MyCoolAgent.ContentEvent', (contentEvent)=>{
     if (contentEvent.message.startsWith('#close')) {
         echoAgent.updateConversationField({
             conversationId: contentEvent.dialogId,
             conversationField: [{
-                    field: "ConversationStateField",
-                    conversationState: "CLOSE"
+                    field: 'ConversationStateField',
+                    conversationState: 'CLOSE'
                 }]
         });
     } else {
         echoAgent.publishEvent({
             dialogId: contentEvent.dialogId,
             event: {
-                type: 'ContentEvent', 
-                contentType: 'text/plain', 
+                type: 'ContentEvent',
+                contentType: 'text/plain',
                 message: `echo : ${contentEvent.message}`
             }
         });
